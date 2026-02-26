11c31a80090461bbc8591f2c101fc4e93219f60c
Code <gitagoswami456@gmail.com>
2026-02-26 11:10:24 +0530

    UC9: Recursive Based Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -3,11 +3,29 @@ import java.util.*;
 public class PalindromeCheckerApp {
 
     /*
-     UC8: Linked List Based Palindrome Checker
+     UC9: Recursive Based Palindrome Checker
 
      @author Rishav
      @version 1.0
      */
+
+    // Recursive method to check palindrome
+    private static boolean check(String s, int start, int end) {
+
+        // Base case: single character or empty string
+        if (start >= end) {
+            return true;
+        }
+
+        // If characters do not match
+        if (s.charAt(start) != s.charAt(end)) {
+            return false;
+        }
+
+        // Recursive call moving towards center
+        return check(s, start + 1, end - 1);
+    }
+
     public static void main(String[] args) {
 
         System.out.println("Welcome to the Palindrome Checker Management System");
@@ -19,26 +37,7 @@ public class PalindromeCheckerApp {
         System.out.print("Input text: ");
         String input = sc.next();
 
-        // LinkedList for double-ended operations
-        LinkedList<Character> list = new LinkedList<>();
-
-        // Add characters to LinkedList
-        for (char c : input.toCharArray()) {
-            list.add(c);
-        }
-
-        boolean isPalindrome = true;
-
-        // Compare front and rear characters
-        while (list.size() > 1) {
-            char first = list.removeFirst();
-            char last = list.removeLast();
-
-            if (first != last) {
-                isPalindrome = false;
-                break;
-            }
-        }
+        boolean isPalindrome = check(input, 0, input.length() - 1);
 
         System.out.println("Is it a Palindrome? : " + isPalindrome);
 
